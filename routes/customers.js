f498d991998d1cff42042cf26a738dd6d9b67677
Tejas Mane <tejasmane485@gmail.com>
2023-02-26 10:53:01 +0530

    signin method

--- routes/customers.js
+++ routes/customers.js
@@ -3,6 +3,9 @@ const { Customers } = require("../sequelize")
 const utils = require("../utils")
 const crypto = require("crypto-js")
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const SECRET = process.env.SECRET
 
 const router = express.Router()
 
@@ -25,6 +28,27 @@ router.post('/signup',(req, res)=>{
         res.send(utils.createResult(null, user))
     }).catch((err) =>{
         res.send(utils.createResult(err, null))
+    })
+})
+
+
+router.post('/signin',(req, res)=>{
+    const {email, password} = req.body
+    const encryptedPassword = crypto.SHA256(password) + ""
+    const result = {}
+    return Customers.findOne({where : {custEmail : email, password: encryptedPassword}}).then(customer =>{
+        const token = jwt.sign({id : user['custId']}, SECRET)
+        result['status'] = 'success'
+        result['data'] = {
+            custFirstName : user['firstName'],
+            custLastName : user['lastName'],
+            token : token
+        }
+        res.json(result)
+    }).catch(err =>{
+        result['status'] = 'error'
+        result['data'] = err
+        res.send(result)
     })
 })
 
